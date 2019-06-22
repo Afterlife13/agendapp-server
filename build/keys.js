117126b509059e1e7d624864a34cf7d31abb6877
Alberto Garcia <alberto96web@gmail.com>
2019-06-22 04:49:04 +0200

    Cambio keys nuevo hosting db

--- build/keys.js
+++ build/keys.js
@@ -2,9 +2,15 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = {
     database: {
+        host: 'remotemysql.com',
+        user: 'beS3Wdezro',
+        password: 'GEW1G0PQGa',
+        database: 'beS3Wdezro'
+        /*
         host: 'sql220.main-hosting.eu',
         user: 'u446997370_alb',
         password: 'alberto123',
         database: 'u446997370_agpp',
+        */
     }
 };
--- src/keys.ts
+++ src/keys.ts
@@ -1,9 +1,15 @@
 export default {
 
     database:{
+        host: 'remotemysql.com',
+        user: 'beS3Wdezro',
+        password: 'GEW1G0PQGa',
+        database: 'beS3Wdezro'
+        /*
         host: 'sql220.main-hosting.eu',
         user: 'u446997370_alb',
         password: 'alberto123',
         database: 'u446997370_agpp',
+        */
     }
 }
